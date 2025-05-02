b7466023dde68bb23b81036abb6fbb554afa432b
IT22277190 <sudeepasandabhanu@gmail.com>
2025-05-03 01:57:41 +0530

    Refactor temperature model schema for consistency and clarity

--- models/temperatureModel.js
+++ models/temperatureModel.js
@@ -1,28 +1,11 @@
-//recommit
 const mongoose = require('mongoose');
-const Joi = require('joi');
-const { USER_TYPES } = require('../constants');
-
 
 const temperatureSchema = new mongoose.Schema({
-  temperature: {
-    type: Number,
-    required: true
-  },
-  humidity: {
-    type: Number,
-    required: true
-  },
-  soil_moisture: {
-    type: Number,
-    required: true
-  },
-  recordedAt: {
-    type: Date,
-    default: Date.now
-  }
+  temperature: { type: Number, required: true },
+  humidity: { type: Number, required: true },
+  soil_moisture: { type: Number, required: true },
+  height: { type: Number, required: true},
+  recordedAt: { type: Date, default: Date.now }
 });
 
-const Temperature = mongoose.model('Temperature', temperatureSchema);
-
-module.exports = Temperature;
+module.exports = mongoose.model('Temperature', temperatureSchema);
