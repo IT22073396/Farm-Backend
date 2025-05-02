9bddb61a65a76871960349789b72104cd82aef9f
IT22277190 <sudeepasandabhanu@gmail.com>
2025-05-03 01:57:21 +0530

    Add AveHour model with schema for temperature, humidity, soil moisture, height, and recorded date

---
+++ models/AveHour.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const avehourSchema = new mongoose.Schema({
+  temperature: { type: Number, required: true },
+  humidity: { type: Number, required: true },
+  soil_moisture: { type: Number, required: true },
+  height: { type: Number, required: true },
+  recordedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Avehours', avehourSchema);
